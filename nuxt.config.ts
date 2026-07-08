0d4c8ea0a662d50918e928e575ccd629ae6e9ea9
DaraCOOKED <dara.somnag020@gmail.com>
2026-07-08 14:37:48 +0700

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,36 +3,31 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  
 
   css: [
     "./app/assets/css/main.css",
     "@fortawesome/fontawesome-free/css/all.min.css",
   ],
 
- vite: {
-  plugins: [tailwindcss()],
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
     },
   },
-},
 
   runtimeConfig: {
-  public: {
-    emailjsServiceId: "service_09csxvs",
-    emailjsTemplateId: "template_kh59r91",
-    emailjsPublicKey: "T8FLPsFfvUtRLmlaa",
-    strapiUrl: "http://localhost:1337",
+    public: {
+      emailjsServiceId: "service_09csxvs",
+      emailjsTemplateId: "template_kh59r91",
+      emailjsPublicKey: "T8FLPsFfvUtRLmlaa",
+      strapiUrl: "http://localhost:1337",
     },
-    
-    
   },
-  },
-},
-  
+
   app: {
     baseURL: "/toothsy-detal-clinic/",
     head: {
@@ -40,6 +35,17 @@ export default defineNuxtConfig({
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@400;500;600;700;800&display=swap",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "50x50",
+          href: "/final-logo.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
         },
       ],
     },

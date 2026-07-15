e3582869fe2ffb0382a35d192408012f5e30e59d
bora4-netizen <chorkbora4@gmail.com>
2026-07-15 10:24:11 +0700

    add nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,32 +3,28 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
   css: [
     "./app/assets/css/main.css",
     "@fortawesome/fontawesome-free/css/all.min.css",
   ],
-
- vite: {
-  plugins: [tailwindcss()],
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
     },
   },
-},
-
   runtimeConfig: {
-  public: {
-    emailjsServiceId: "service_09csxvs",
-    emailjsTemplateId: "template_kh59r91",
-    emailjsPublicKey: "T8FLPsFfvUtRLmlaa",
-
-    strapiUrl: "http://localhost:1337",
+    public: {
+      emailjsServiceId: "service_09csxvs",
+      emailjsTemplateId: "template_kh59r91",
+      emailjsPublicKey: "T8FLPsFfvUtRLmlaa",
+      strapiUrl: "http://localhost:1337",
+      apiBase: "http://localhost:8000/api",
+    },
   },
-},
-  
   app: {
     baseURL: "/toothsy-detal-clinic/",
     head: {
